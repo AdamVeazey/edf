da89da66b720225eca71046a8d6145b6e4a716b6
Adam Veazey <adamveazey21@gmail.com>
2023-05-25 19:47:33 -0500

    formatting

--- src/EDF/Vector.hpp
+++ src/EDF/Vector.hpp
@@ -30,37 +30,37 @@ public:
     using ConstReverseIterator = std::reverse_iterator<ConstIterator>;
 
      /* Current state */
-    inline constexpr bool isEmpty()                             const { return n == 0; }
-    inline constexpr bool isFull()                              const { return n == N; }
+    inline constexpr bool isEmpty()                                                         const { return n == 0; }
+    inline constexpr bool isFull()                                                          const { return n == N; }
 
      /* Capacity */
-    inline constexpr const std::size_t& length()                const { return n; }
-    inline constexpr std::size_t maxLength()                    const { return N; }
+    inline constexpr const std::size_t& length()                                            const { return n; }
+    inline constexpr std::size_t maxLength()                                                const { return N; }
 
     /* Element access */
-    inline constexpr T& at( std::size_t index )                       { return buffer.at( index ); }
-    inline constexpr const T& at( std::size_t index )           const { return buffer.at( index ); }
+    inline constexpr T& at( std::size_t index )                                                   { return buffer.at( index ); }
+    inline constexpr const T& at( std::size_t index )                                       const { return buffer.at( index ); }
 
-    inline constexpr T& operator[]( std::size_t index )               { return buffer[index]; }
-    inline constexpr const T& operator[]( std::size_t index )   const { return buffer[index]; }
+    inline constexpr T& operator[]( std::size_t index )                                           { return buffer[index]; }
+    inline constexpr const T& operator[]( std::size_t index )                               const { return buffer[index]; }
     
-    inline constexpr T& front()                                       { return at( 0 ); }
-    inline constexpr const T& front()                           const { return at( 0 ); }
+    inline constexpr T& front()                                                                   { return at( 0 ); }
+    inline constexpr const T& front()                                                       const { return at( 0 ); }
     
-    inline constexpr T& back()                                        { return at( n - 1 ); }
-    inline constexpr const T& back()                            const { return at( n - 1 ); }
+    inline constexpr T& back()                                                                    { return at( n - 1 ); }
+    inline constexpr const T& back()                                                        const { return at( n - 1 ); }
     
-    inline constexpr T* data()                                        { return buffer.data(); }
-    inline constexpr const T* data()                            const { return buffer.data(); }
+    inline constexpr T* data()                                                                    { return buffer.data(); }
+    inline constexpr const T* data()                                                        const { return buffer.data(); }
 
     /* Operations */
-    inline constexpr void clear()                                                               { std::destroy( begin(), end() ); n = 0; }
+    inline constexpr void clear()                                                           { std::destroy( begin(), end() ); n = 0; }
 
-    inline constexpr void insert( std::size_t index, const T& value )                           { insert( ConstIterator(data() + index), value ); }
-    inline constexpr void insert( std::size_t index, T&& value )                                { insert( ConstIterator(data() + index), value ); }
-    inline constexpr void insert( std::size_t index, std::size_t count, const T& value )        { insert( ConstIterator(data() + index), count, value ); }
-    inline constexpr void insert( std::size_t index, std::initializer_list<T> iList )           { insert( ConstIterator(data() + index), iList ); }
-    inline constexpr Iterator insert( ConstIterator pos, const T& value )                       { return insert( pos, 1, value ); }
+    inline constexpr void insert( std::size_t index, const T& value )                       { insert( ConstIterator(data() + index), value ); }
+    inline constexpr void insert( std::size_t index, T&& value )                            { insert( ConstIterator(data() + index), value ); }
+    inline constexpr void insert( std::size_t index, std::size_t count, const T& value )    { insert( ConstIterator(data() + index), count, value ); }
+    inline constexpr void insert( std::size_t index, std::initializer_list<T> iList )       { insert( ConstIterator(data() + index), iList ); }
+    inline constexpr Iterator insert( ConstIterator pos, const T& value )                   { return insert( pos, 1, value ); }
     constexpr Iterator insert( ConstIterator pos, T&& value);
     constexpr Iterator insert( ConstIterator pos, std::size_t count, const T& value );
     constexpr Iterator insert( ConstIterator pos, std::initializer_list<T> iList );
@@ -68,36 +68,36 @@ public:
     template<typename... Args>
     constexpr Iterator emplace( ConstIterator pos, Args&&... args );
     
-    inline constexpr void erase( std::size_t index )                                            { erase( ConstIterator(data() + index) ); }
-    inline constexpr void erase( std::size_t first, std::size_t last )                          { erase( ConstIterator(data() + first), ConstIterator(data() + last) ); }
+    inline constexpr void erase( std::size_t index )                                        { erase( ConstIterator(data() + index) ); }
+    inline constexpr void erase( std::size_t first, std::size_t last )                      { erase( ConstIterator(data() + first), ConstIterator(data() + last) ); }
     constexpr Iterator erase( ConstIterator pos );
     constexpr Iterator erase( ConstIterator first, ConstIterator last );
 
-    inline constexpr void pushBack( const T& value )                                            { insert( end(), value ); }
-    inline constexpr void pushBack( T&& value )                                                 { emplaceBack(std::move(value) ); }
+    inline constexpr void pushBack( const T& value )                                        { insert( end(), value ); }
+    inline constexpr void pushBack( T&& value )                                             { emplaceBack(std::move(value) ); }
 
     template<typename... Args>
-    inline constexpr T& emplaceBack(Args&&... args) { return *emplace(end(), args...); }
+    inline constexpr T& emplaceBack(Args&&... args)                                         { return *emplace(end(), args...); }
 
-    inline constexpr T popBack()                                                                { EDF_ASSERTD(!isEmpty()); T v(std::move(back())); buffer[n--].~T(); return v; }
+    inline constexpr T popBack()                                                            { EDF_ASSERTD(!isEmpty()); T v(std::move(back())); buffer[n--].~T(); return v; }
 
 
     /* Iterators */
-    inline constexpr Iterator begin()                                           { return Iterator( data() ); }
-    inline constexpr ConstIterator begin()                                const { return ConstIterator( data() ); }
-    inline constexpr ConstIterator cbegin()                               const { return ConstIterator( data() ); }
+    inline constexpr Iterator begin()                                                             { return Iterator( data() ); }
+    inline constexpr ConstIterator begin()                                                  const { return ConstIterator( data() ); }
+    inline constexpr ConstIterator cbegin()                                                 const { return ConstIterator( data() ); }
 
-    inline constexpr Iterator end()                                             { return Iterator( data() + n ); }
-    inline constexpr ConstIterator end()                                  const { return ConstIterator( data() + n ); }
-    inline constexpr ConstIterator cend()                                 const { return ConstIterator( data() + n ); }
+    inline constexpr Iterator end()                                                               { return Iterator( data() + n ); }
+    inline constexpr ConstIterator end()                                                    const { return ConstIterator( data() + n ); }
+    inline constexpr ConstIterator cend()                                                   const { return ConstIterator( data() + n ); }
 
-    inline constexpr ReverseIterator rbegin()                                   { return ReverseIterator( end() ); }
-    inline constexpr ConstReverseIterator rbegin()                        const { return ConstReverseIterator( end() ); }
-    inline constexpr ConstReverseIterator crbegin()                       const { return ConstReverseIterator( end() ); }
+    inline constexpr ReverseIterator rbegin()                                                     { return ReverseIterator( end() ); }
+    inline constexpr ConstReverseIterator rbegin()                                          const { return ConstReverseIterator( end() ); }
+    inline constexpr ConstReverseIterator crbegin()                                         const { return ConstReverseIterator( end() ); }
 
-    inline constexpr ReverseIterator rend()                                     { return ReverseIterator( begin() ); }
-    inline constexpr ConstReverseIterator rend()                          const { return ConstReverseIterator( begin() ); }
-    inline constexpr ConstReverseIterator crend()                         const { return ConstReverseIterator( begin() ); }
+    inline constexpr ReverseIterator rend()                                                       { return ReverseIterator( begin() ); }
+    inline constexpr ConstReverseIterator rend()                                            const { return ConstReverseIterator( begin() ); }
+    inline constexpr ConstReverseIterator crend()                                           const { return ConstReverseIterator( begin() ); }
 };
 
 /* Non-member functions */
