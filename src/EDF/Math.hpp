413a86286e87dd3b3e7af3beb508d504d5833d4d
Adam Veazey <adamveazey21@gmail.com>
2023-06-18 00:51:36 -0500

    Add initial math functions

--- src/EDF/Math.hpp
+++ src/EDF/Math.hpp
@@ -8,11 +8,64 @@
 #pragma once
 
 #include <cstddef>
+#include <cmath>
 
 // NOTE: This needs to be in the global namespace. EX: 0_uz == std::size_t literal 
 inline std::size_t operator ""_uz( unsigned long long x ) { return x; }
 
 namespace EDF {
 
+template<typename T, std::size_t N>
+inline constexpr std::size_t nElements( const T (&)[N] ) { return N; }
+
+// tag::is_pow_2[]
+inline constexpr bool isPow2( std::size_t v ) { return !(v&(v-1)); }
+// end:;is_pow_2[]
+
+template<typename T> 
+inline constexpr const T& min( const T& lhs, const T& rhs ) { 
+    return (lhs < rhs) ? lhs : rhs; 
+}
+
+template<typename T> 
+inline constexpr const T& max( const T& lhs, const T& rhs ) { 
+    return (lhs > rhs) ? lhs : rhs; 
+}
+
+template<std::size_t R1, std::size_t R2, typename T>
+inline constexpr T voltageDividerCalcVout( const T& Vin ){
+    const auto Vout = (Vin * static_cast<T>(R2)) / static_cast<T>(R1 + R2);
+    if constexpr( std::is_floating_point_v<T> ) {
+        return std::round( Vout );
+    }
+    return Vout;
+}
+
+template<std::size_t R1, std::size_t R2, typename T>
+inline constexpr T voltageDividerCalcVin( const T& Vout ){
+    const auto Vin = (Vout * (R1 + R2)) / R2;
+    if constexpr( std::is_floating_point_v<T> ) {
+        return std::round( Vin );
+    }
+    return Vin;
+}
+
+template<typename T>
+inline constexpr T temperatureCtoF( const T& temp_C ) {
+    const auto f = ((temp_C * 9) / (5)) + 32;
+    if constexpr( std::is_floating_point_v<T> ){
+        return std::round(f);
+    }
+    return f;
+}
+
+template<typename T>
+inline constexpr T temperatureFtoC( const T& temp_F ) {
+    const auto c = ((temp_F - 32) * 5) / 9;
+    if constexpr( std::is_floating_point_v<T> ) {
+        return std::round(c);
+    }
+    return c;
+}
 
 } /* EDF */
