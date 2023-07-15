143a3bae47c1ebc68be8d953af10fb31e07ab75b
Adam Veazey <adamveazey21@gmail.com>
2023-07-15 01:22:46 -0500

    Add Color and Color565

---
+++ examples/EDF/Color/main.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2023, Adam Veazey
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <EDF/Color.hpp>
+
+#include <iostream>
+
+
+void example() {
+    // tag::init_default[]
+    EDF::Color black;
+    // end::init_default[]
+
+    // tag::init_initial_value[]
+    EDF::Color salmon( 0xFA8072 );
+    // end::init_initial_value[]
+
+    // tag::init_rgba[]
+    EDF::Color white( 255, 255, 255 );
+    EDF::Color gray( 128, 128, 128, 255 );
+    // end::init_rgba[]
+
+    // tag::set_rgba[]
+    black.setR( 255 );  // turn red
+    black.setG( 255 );  // turn yellow
+    black.setB( 255 );  // turn white
+    black.setA( 0 );    // turn transparent
+    // end::set_rgba[]
+
+    // tag::get_rgba[]
+    salmon.r(); // 0xFA
+    salmon.g(); // 0x80
+    salmon.b(); // 0x72
+    salmon.a(); // 0xFF
+    // end::get_rgba[]
+
+    // tag::get_as[]
+    white.asRGB();  // 0xFFFFFF
+    gray.asRGBA();  // 0xFFFFFFFF
+    // end::get_as[]
+
+    // tag::named_colors[]
+    EDF::Color::red();
+    EDF::Color::springGreen();
+    EDF::Color::cyan();
+    // end::named_colors[]
+}
+
+// tag::example_565_1[]
+void function( const EDF::Color565& color ) {
+    /* ... */
+}
+// end::example_565_1[]
+
+void example565() {
+    // tag::init_default_565[]
+    EDF::Color565 black;
+    // end::init_default_565[]
+
+    // tag::init_initial_value_565[]
+    EDF::Color565 salmon( 0xFC0E );
+    // end::init_initial_value_565[]
+
+    // tag::init_rgba_565[]
+    EDF::Color565 white( 255, 255, 255 );
+    // end::init_rgba_565[]
+
+    // tag::init_color_565[]
+    EDF::Color565 color( EDF::Color::blueViolet() );
+    // end::init_color_565[]
+
+    // tag::set_rgba_565[]
+    black.setR( 255 );  // turn red
+    black.setG( 255 );  // turn yellow
+    black.setB( 255 );  // turn white
+    // end::set_rgba_565[]
+
+    // tag::get_rgba_565[]
+    salmon.r(); // 0xF8
+    salmon.g(); // 0x80
+    salmon.b(); // 0x70
+    // end::get_rgba_565[]
+
+    // tag::get_as_565[]
+    white.asRGB();  // 0xFFFF
+    // end::get_as_565[]
+
+    // tag::example_565_2[]
+    function( EDF::Color::magenta() );
+    // end::example_565_2[]
+}
+
+int main( void ) {
+
+    EDF::Color565 c( EDF::Color( 0xFA8072 ) );
+    std::cout << std::hex;
+    std::cout << "r: " << +c.r() << '\n'
+              << "g: " << +c.g() << '\n'
+              << "b: " << +c.b() << '\n';
+    // std::cout << "a: " << +c.a() << '\n';
+    // c.setG( 0 );
+    uint32_t value = c.asRGB();
+    std::cout << "0x" << value << std::endl;
+
+    return 0;
+}
---
+++ src/EDF/Color.hpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2023, Adam Veazey
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "EDF/BitField.hpp"
+
+namespace EDF {
+
+class Color {
+private:
+    BitField32 rgba;
+public:
+    constexpr Color( uint32_t initialValue = 0 ) : rgba( initialValue ) {}
+    constexpr Color( uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF ) :
+        rgba( (a << 24) | (r << 16) | (g << 8) | b )
+    {}
+
+    /* Set color components */
+    constexpr void setR( uint8_t r )          { rgba.set( 16, 8, r ); }
+    constexpr void setG( uint8_t g )          { rgba.set(  8, 8, g ); }
+    constexpr void setB( uint8_t b )          { rgba.set(  0, 8, b ); }
+    constexpr void setA( uint8_t a )          { rgba.set( 24, 8, a ); }
+
+    /* Get color components */
+    constexpr uint8_t r()               const { return rgba.get( 16, 8 ); }
+    constexpr uint8_t g()               const { return rgba.get( 8 , 8 ); }
+    constexpr uint8_t b()               const { return rgba.get(  0, 8 ); }
+    constexpr uint8_t a()               const { return rgba.get( 24, 8 ); }
+
+    constexpr uint32_t asRGBA()         const { return rgba.get( 0, 32 ); }
+    constexpr uint32_t asRGB()          const { return rgba.get( 0, 24 ); }
+
+    /* Named colors */
+    // tag::named_colors[]
+    static constexpr Color red()        { return Color( 255,   0,   0 ); }
+    static constexpr Color orange()     { return Color( 255, 128,   0 ); }
+    static constexpr Color yellow()     { return Color( 255, 255,   0 ); }
+    static constexpr Color lime()       { return Color( 128, 255,   0 ); }
+    static constexpr Color green()      { return Color(   0, 255,   0 ); }
+    static constexpr Color springGreen(){ return Color(   0, 255, 128 ); }
+    static constexpr Color cyan()       { return Color(   0, 255, 255 ); }
+    static constexpr Color dodgerBlue() { return Color(   0, 128, 255 ); }
+    static constexpr Color blue()       { return Color(   0,   0, 255 ); }
+    static constexpr Color blueViolet() { return Color( 128,   0, 255 ); }
+    static constexpr Color magenta()    { return Color( 255,   0, 255 ); }
+    static constexpr Color deepPink()   { return Color( 255,   0, 128 ); }
+
+ 	static constexpr Color black()	    { return Color(   0,   0,   0 ); }
+ 	static constexpr Color dimGray()	{ return Color( 105, 105, 105 ); }
+ 	static constexpr Color gray()	    { return Color( 128, 128, 128 ); }
+ 	static constexpr Color darkGray()	{ return Color( 169, 169, 169 ); }
+ 	static constexpr Color silver()	    { return Color( 192, 192, 192 ); }
+ 	static constexpr Color lightGray()	{ return Color( 211, 211, 211 ); }
+ 	static constexpr Color gainsboro()	{ return Color( 220, 220, 220 ); }
+ 	static constexpr Color whiteSmoke()	{ return Color( 245, 245, 245 ); }
+ 	static constexpr Color white()	    { return Color( 255, 255, 255 ); }
+    // end::named_colors[]
+};
+
+class Color565 {
+private:
+    BitField16 rgb;
+public:
+    constexpr Color565( uint16_t initialValue = 0 ) : rgb( initialValue ) {}
+    constexpr Color565( uint8_t r, uint8_t g, uint8_t b ) :
+        rgb( ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3) )
+    {}
+    constexpr Color565( const Color& c ) :
+        rgb( ((c.r() >> 3) << 11) | ((c.g() >> 2) << 5) | (c.b() >> 3) )
+    {}
+
+    /* Set color components */
+    constexpr void setR( uint8_t r )          { rgb.set( 11, 5, r >> 3 ); }
+    constexpr void setG( uint8_t g )          { rgb.set(  5, 6, g >> 2 ); }
+    constexpr void setB( uint8_t b )          { rgb.set(  0, 5, b >> 3 ); }
+
+    /* Get color components */
+    constexpr uint8_t r()               const { return rgb.get( 11, 5 ) << 3; }
+    constexpr uint8_t g()               const { return rgb.get(  5, 6 ) << 2; }
+    constexpr uint8_t b()               const { return rgb.get(  0, 5 ) << 3; }
+
+    constexpr uint16_t asRGB()          const { return rgb.get( 0, 16 ); }
+};
+
+} /* EDF */
