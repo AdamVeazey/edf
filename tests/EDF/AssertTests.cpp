c1bc2bb6d1aee09b6ac7b5bb45cd84bf22bd76be
Adam Veazey <adamveazey21@gmail.com>
2023-07-25 08:49:39 -0500

    Add Assert tests

--- tests/EDF/AssertTests.cpp
+++ tests/EDF/AssertTests.cpp
@@ -7,3 +7,31 @@
 #include <EDF/Assert.hpp>
 
 #include <gtest/gtest.h>
+
+
+TEST(Assert, AssertionPasses) {
+    EDF_ASSERT(true);
+}
+
+TEST(Assert, AssertionFails) {
+    ASSERT_DEATH(EDF_ASSERT(false), "");
+}
+
+TEST(Assert, DebugAssertionPasses) {
+#ifndef NDEBUG
+    // Expecting that this condition should pass, so the program should not terminate.
+    EDF_ASSERTD(true);
+#else
+    // When NDEBUG is not defined, EDF_ASSERTD is a no-op, so this test will pass regardless of the condition.
+    SUCCEED();
+#endif
+}
+
+TEST(Assert, DebugAssertionFails) {
+#ifndef NDEBUG
+    ASSERT_DEATH(EDF_ASSERTD(false), "");
+#else
+    // When NDEBUG is not defined, EDF_ASSERTD is a no-op, so this test will pass regardless of the condition.
+    SUCCEED();
+#endif
+}
