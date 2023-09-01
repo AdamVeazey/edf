909e5c6eaa48dd16ac9a0d3b3caef8a5a1178f2a
Adam Veazey <adamveazey21@gmail.com>
2023-08-31 23:42:50 -0500

    Add to integral tests

--- tests/EDF/StringTests.cpp
+++ tests/EDF/StringTests.cpp
@@ -354,3 +354,50 @@ TEST(String, AsByteData) {
     EXPECT_EQ( string.asByteData()[1], 'o' );
 }
 
+TEST(String, ToIntegralBase10) {
+    EXPECT_EQ( EDF::String<16>("-55").toInt8_t(), -55 );
+    EXPECT_EQ( EDF::String<16>("-5566").toInt16_t(), -5566 );
+    EXPECT_EQ( EDF::String<16>("-556677788").toInt32_t(), -556677788 );
+    EXPECT_EQ( EDF::String<32>("-5566777889999999999").toInt64_t(), -5566777889999999999ll );
+
+    EXPECT_EQ( EDF::String<16>("55").toUint8_t(), 55 );
+    EXPECT_EQ( EDF::String<16>("5566").toUint16_t(), 5566 );
+    EXPECT_EQ( EDF::String<16>("556677788").toUint32_t(), 556677788 );
+    EXPECT_EQ( EDF::String<32>("5566777889999999999").toUint64_t(), 5566777889999999999ll );
+}
+
+TEST(String, ToIntegralBase16) {
+    EXPECT_EQ( EDF::String<16>("C9").toInt8_t( 16 ), -55 );
+    EXPECT_EQ( EDF::String<16>("EA42").toInt16_t( 16 ), -5566 );
+    EXPECT_EQ( EDF::String<16>("DED1C564").toInt32_t( 16 ), -556677788 );
+    EXPECT_EQ( EDF::String<32>("B2BED500CB0D2C01").toInt64_t( 16 ), -5566777889999999999ll );
+
+    EXPECT_EQ( EDF::String<16>("37").toUint8_t( 16 ), 55 );
+    EXPECT_EQ( EDF::String<16>("15BE").toUint16_t( 16 ), 5566 );
+    EXPECT_EQ( EDF::String<16>("212E3A9C").toUint32_t( 16 ), 556677788 );
+    EXPECT_EQ( EDF::String<32>("4D412AFF34F2D3FF").toUint64_t( 16 ), 5566777889999999999ll );
+}
+
+TEST(String, ToIntegralBase2) {
+    EXPECT_EQ( EDF::String<9>("11001001").toInt8_t( 2 ), -55 );
+    EXPECT_EQ( EDF::String<17>("1110101001000010").toInt16_t( 2 ), -5566 );
+    EXPECT_EQ( EDF::String<33>("11011110110100011100010101100100").toInt32_t( 2 ), -556677788 );
+    EXPECT_EQ( EDF::String<65>("1011001010111110110101010000000011001011000011010010110000000001").toInt64_t( 2 ), -5566777889999999999ll );
+
+    EXPECT_EQ( EDF::String<9>("00110111").toUint8_t( 2 ), 55 );
+    EXPECT_EQ( EDF::String<17>("0001010110111110").toUint16_t( 2 ), 5566 );
+    EXPECT_EQ( EDF::String<33>("00100001001011100011101010011100").toUint32_t( 2 ), 556677788 );
+    EXPECT_EQ( EDF::String<65>("0100110101000001001010101111111100110100111100101101001111111111").toUint64_t( 2 ), 5566777889999999999ll );
+}
+
+TEST(String, ToIntegralBase8) {
+    EXPECT_EQ( EDF::String<16>("311").toInt8_t( 8 ), -55 );
+    EXPECT_EQ( EDF::String<16>("165102").toInt16_t( 8 ), -5566 );
+    EXPECT_EQ( EDF::String<16>("33664342544").toInt32_t( 8 ), -556677788 );
+    EXPECT_EQ( EDF::String<32>("1312766520031303226001").toInt64_t( 8 ), -5566777889999999999ll );
+
+    EXPECT_EQ( EDF::String<16>("67").toUint8_t( 8 ), 55 );
+    EXPECT_EQ( EDF::String<16>("12676").toUint16_t( 8 ), 5566 );
+    EXPECT_EQ( EDF::String<16>("4113435234").toUint32_t( 8 ), 556677788 );
+    EXPECT_EQ( EDF::String<32>("465011257746474551777").toUint64_t( 8 ), 5566777889999999999ll );
+}
