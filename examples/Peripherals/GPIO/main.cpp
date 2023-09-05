4db366da85913398e1ee9ffd96b93722e12b96e0
Adam Veazey <adamveazey21@gmail.com>
2023-09-04 21:27:28 -0500

    Add GPIO interface, documentation, and example

---
+++ examples/Peripherals/GPIO/main.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2023, Adam Veazey
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <EDF/Peripherals/GPIO.hpp>
+
+#include <iostream>
+
+// tag::mock_gpio[]
+/* Simulate a GPIO memory mapped peripheral, here is just a single "register" */
+struct MCU_GPIO_T {
+    volatile uint32_t DATA_REGISTER;
+};
+
+/* For this example, we need a real instance of the fake memory mapped peripheral */
+static MCU_GPIO_T MCU_GPIO_PORTA;
+
+/* Normally the definition is a hardcoded address that comes from the datasheet */
+#define GPIO_PORTA (&MCU_GPIO_PORTA)
+
+class GPIO final : public EDF::GPIO {
+public:
+    using Level = EDF::GPIO::Level;
+private:
+    MCU_GPIO_T* port;
+    uint32_t pin;
+public:
+    GPIO( MCU_GPIO_T* port, uint32_t pin ) : port(port), pin(1 << pin) {}
+    virtual ~GPIO() = default;
+
+    /* Initialization functions for the specific  */
+    void configureAsInput() {}
+    void configureAsOutput() {}
+
+    virtual Level getLevel() const override {
+        return static_cast<Level>((port->DATA_REGISTER & pin) != 0);
+    }
+    virtual void set( Level level ) const override {
+        if( level == Level::HIGH ) {
+            port->DATA_REGISTER |= pin;
+        }
+        else {
+            port->DATA_REGISTER &= ~pin;
+        }
+    }
+};
+// end::mock_gpio[]
+
+int main() {
+    using namespace std;
+
+    // tag::init[]
+    GPIO pin( GPIO_PORTA, 5 );
+    // end::init[]
+
+    // tag::get_level[]
+    if( pin.getLevel() == GPIO::Level::LOW ) {
+        // ...
+    }
+    // end::get_level[]
+
+    // tag::is_low[]
+    if( pin.isLow() ) {
+        // ...
+    }
+    // end::is_low[]
+
+    // tag::is_high[]
+    if( pin.isHigh() ) {
+        // ...
+    }
+    // end::is_high[]
+
+    // tag::set[]
+    pin.set( GPIO::Level::LOW );
+    // end::set[]
+
+    // tag::toggle[]
+    pin.toggle();
+    // end::toggle[]
+
+    // tag::set_low[]
+    pin.setLow();
+    // end::set_low[]
+
+    // tag::set_high[]
+    pin.setHigh();
+    // end::set_high[]
+
+    for( int k = 0; k < 5; ++k ) {
+        cout << "level: " << (pin.isHigh() ? "High" : "Low") << endl;
+        pin.toggle();
+    }
+
+    return 0;
+}
---
+++ src/EDF/Peripherals/GPIO.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023, Adam Veazey
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace EDF {
+
+class GPIO {
+public:
+    enum class Level : uint8_t {
+        LOW,
+        HIGH
+    };
+public:
+    virtual ~GPIO() = default;
+
+    /* Input member functions */
+    virtual Level getLevel()    const = 0;
+    inline bool isLow()         const { return getLevel() == Level::LOW; }
+    inline bool isHigh()        const { return getLevel() == Level::HIGH; }
+
+    /* Output member functions */
+    virtual void set( Level level ) const = 0;
+    virtual void toggle()       const { set( isHigh() ? Level::LOW : Level::HIGH ); }
+    inline void setLow()        const { set( Level::LOW ); }
+    inline void setHigh()       const { set( Level::HIGH ); }
+};
+
+} /* EDF */
